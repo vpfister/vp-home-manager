0b8068cca32499168bf5b59626f8b040e83651a0
vincent <vincent@famille-pfister.net>
2025-02-24 22:41:29 +0100

    add gnome config

--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
 { lib, pkgs, ... }: let
     username = "vincent";
     email = "vincent@famille-pfister.net";
+    wallpaperImg = pkgs.fetchurl {
+        url = "https://images8.alphacoders.com/137/1374345.jpg";
+        hash = "sha256:f356cd6c4f516562614a2bd20596d72926ca619f185ef484613e4af0e7a52943";
+    };
 in {
   home = {
     packages = with pkgs; [
@@ -34,6 +38,23 @@ in {
             extraConfig = {
                 init.defaultBranch = "main";
                 core.editor = "nvim";
+            };
+        };
+    };
+
+    dconf = {
+        enable = true;
+
+        settings = {
+            "org/gnome/desktop/interface" = {
+                color-scheme = "prefer-dark";
+            };
+            "org/gnome/desktop/background" = {
+                picture-uri = "file://${wallpaperImg}";
+                picture-uri-dark = "file://${wallpaperImg}";
+            };
+            "org/gnome/desktop/screensaver" = {
+                picture-uri = "file://${wallpaperImg}";
             };
         };
     };
